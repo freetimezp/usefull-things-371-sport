74a88736692713ca837af0a33dd0ba8cfe8592a4
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-10-24 00:03:53 +0300

    10. usefull-things-371-sport. Calculate BMI func.

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+//calculate bmi
+const calculateForm = document.getElementById("calculate-form");
+const calculateCm = document.getElementById("calculate-cm");
+const calculateKg = document.getElementById("calculate-kg");
+const calculateMessage = document.getElementById("calculate-message");
+
+function calculateBmi(e) {
+    e.preventDefault();
+
+    if (calculateCm.value === "" || calculateKg.value === "") {
+        calculateMessage.classList.remove("color-first");
+        calculateMessage.classList.add("color-red");
+
+        calculateMessage.textContent = "Please, fill all values Height & Weight";
+
+        setTimeout(() => {
+            calculateMessage.textContent = "";
+        }, 3000);
+    } else {
+        const cm = calculateCm.value / 100;
+        const kg = calculateKg.value;
+        const bmi = Math.round(kg / (cm * cm));
+
+        if (bmi < 18.5) {
+            calculateMessage.classList.add("color-first");
+            calculateMessage.textContent = `Your BMI is - ${bmi} - you are skinny.`;
+        } else if (bmi < 25) {
+            calculateMessage.classList.add("color-first");
+            calculateMessage.textContent = `Your BMI is - ${bmi} - you are healthy.`;
+        } else {
+            calculateMessage.classList.add("color-first");
+            calculateMessage.textContent = `Your BMI is - ${bmi} - you are little heavy.`;
+        }
+
+        setTimeout(() => {
+            calculateCm.value = "";
+            calculateKg.value = "";
+            calculateMessage.textContent = "";
+        }, 3000);
+    }
+}
+
+calculateForm.addEventListener("submit", calculateBmi);
